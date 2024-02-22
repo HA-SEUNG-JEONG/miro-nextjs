db1914ed8f9189a100575e4e9ddbb2712110e274
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-02-22 22:19:26 +0900

    Chore : convex, clerk 라이브러리 세팅

---
+++ components/ui/auth/loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+    return (
+        <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
+            Loading..
+        </div>
+    );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://verified-turkey-44.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+};
