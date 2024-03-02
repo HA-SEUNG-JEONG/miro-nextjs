fcaa80bb1c325209022418fd1481033f1a9aec28
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-03-02 21:51:30 +0900

    Fix : 라우팅 경로 수정

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -2,6 +2,7 @@
 
 import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
+import { useApiMutation } from "@/use-api-mutation";
 import { useMutation } from "convex/react";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -14,17 +15,17 @@ interface NewButtonProps {
 
 const NewBoardButton = ({ orgId, disabled }: NewButtonProps) => {
     const router = useRouter();
-    const create = useMutation(api.board.create);
+    const { mutate, pending } = useApiMutation(api.board.create);
     const onClick = () => {
-        create({
+        mutate({
             orgId,
-            title: "New Board"
+            title: "Untitled"
         })
             .then((id) => {
-                toast.success("board created");
-                router.push(`/boards/${id}`);
+                toast.success("Board created");
+                router.push(`/board/${id}`);
             })
-            .catch(() => toast.error("Board creation failed"));
+            .catch(() => toast.error("Failed to create board"));
     };
 
     return (
